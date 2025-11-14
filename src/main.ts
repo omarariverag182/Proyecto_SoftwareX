dcf1793274b0aaceb0ab0343c43b689f1cbc6a3e
NEQV78 <nicestquinonesv@unimonserrate.edu.co>
2025-11-14 17:15:38 -0500

    Delete src directory

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-import { AppRoot } from './app/app-root';
-import { routes } from './app/app.routes';
-
-bootstrapApplication(AppRoot, {
-  providers: [ provideRouter(routes) ]
-}).catch(err => console.error(err));
